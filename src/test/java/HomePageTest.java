c5f406e0b1a081bee9ab7b4df9c0c98fcdfa2464
Krzysztof Waliszewski <krzysztof.waliszewski87@gmail.com>
2021-10-14 14:31:30 +0200

    New Project Selenium

---
+++ src/test/java/HomePageTest.java
@@ -0,0 +1,44 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+
+public class HomePageTest {
+
+    private WebDriver driver;
+
+
+    @BeforeAll
+    public static void setUp(){
+        WebDriverManager.chromedriver().setup();
+    }
+
+    @BeforeEach
+    public void setupTest() {
+        driver = new ChromeDriver();
+    }
+
+    @AfterEach
+    public void tearDown() {
+        if (driver != null) {
+            driver.quit();
+        }
+    }
+
+    @Test
+    public void shouldReturnCorrectPage() {
+        driver.get("http://automationpractice.com/index.php");
+
+        Assertions.assertThat(driver.getTitle()).isEqualTo("My Store");
+    }
+
+
+
+
+
+}
